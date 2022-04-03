6fc5660502e8bb80b103ef07f1f6010493969a69
patrickbuerk <98759563+patrickbuerk@users.noreply.github.com>
2022-04-03 16:08:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String partyAffiliationD ="D" ;
+        String partyAffiliationR = "R";
+        String partyAffiliationI = "I";
+        String partyAffiliationO = "O";
+        if (partyAffiliationO.equalsIgnoreCase("D")){
+            System.out.println("You get a Democratic Donkey");
+        }
+        else if (partyAffiliationO.equalsIgnoreCase("R")){
+            System.out.println("you get a republic Elephant");
+        }
+        else if (partyAffiliationO.equalsIgnoreCase("I")){
+            System.out.println("you get an independent Eagle ");
+        }
+        else if (partyAffiliationO.equalsIgnoreCase("O")){
+            System.out.println("you get an Other sloth");
+        }
+        if (partyAffiliationI.equalsIgnoreCase("D")){
+            System.out.println("You get a Democratic Donkey");
+        }
+        else if (partyAffiliationI.equalsIgnoreCase("R")){
+            System.out.println("you get a republic Elephant");
+        }
+        else if (partyAffiliationI.equalsIgnoreCase("I")){
+            System.out.println("you get an independent Eagle ");
+        }
+        else if (partyAffiliationI.equalsIgnoreCase("O")){
+            System.out.println("you get an Other sloth");
+        }
+
+        if (partyAffiliationR.equalsIgnoreCase("D")){
+            System.out.println("You get a Democratic Donkey");
+        }
+        else if (partyAffiliationR.equalsIgnoreCase("R")){
+            System.out.println("you get a republic Elephant");
+        }
+        else if (partyAffiliationR.equalsIgnoreCase("I")){
+            System.out.println("you get an independent Eagle ");
+        }
+        else if (partyAffiliationR.equalsIgnoreCase("O")){
+            System.out.println("you get an Other sloth");
+        }
+
+
+        if (partyAffiliationD.equalsIgnoreCase("D")) {
+            System.out.println("You get a Democratic Donkey");
+        }
+            else if (partyAffiliationD.equalsIgnoreCase("R")){
+
+                System.out.println("you get a Republic Elephant");}
+
+
+                else if (partyAffiliationD.equalsIgnoreCase("I")){
+                    System.out.println("You get an Independent Eagle");}
+                    else if (partyAffiliationD.equalsIgnoreCase("O")){
+                        System.out.println("you get an Other sloth!");
+                    }
+                }
+        }
+
+
+
+
+
+
